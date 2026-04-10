02585de2c603b23d147daec5d592776d3464b364
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-04-10 16:51:19 +0200

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,15 +1,12 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
     namespace = "com.sabaileltaweel.app"
     compileSdk = flutter.compileSdkVersion
-
-    // Override NDK version to match plugin requirements (path_provider_android needs 27.0.12077973)
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -19,11 +16,11 @@ android {
     }
 
     kotlinOptions {
-         jvmTarget = "17" 
+        jvmTarget = "17"
     }
 
     defaultConfig {
-        applicationId = "com.example.sabaileltaweel"
+        applicationId = "com.sabaileltaweel.app"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = 3
@@ -32,7 +29,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
